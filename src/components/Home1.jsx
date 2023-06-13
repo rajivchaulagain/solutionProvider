dfb905ad3d858cb1bf6cc8d99c900164c5924a8b
Rajiv Chaulagain <82865638+rajivchaulagain@users.noreply.github.com>
2023-06-13 19:35:47 +0545

    header changed

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './home1.css';
+import headerImg from './office1.png'
 
 export const Home1 = () => {
   const [posts, setPosts] = useState([]);
@@ -33,12 +34,24 @@ export const Home1 = () => {
   };
 
   return (
-    <div>
-      <h1 className="heading">Find Solution For Your Problem</h1>
-
-      <h2 className="headPost">Post your Problem</h2>
+    <div className='header-banner'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-6'>
+      <h1 className="heading">Find Services
+      <br />
+        <span>
+          Post your Problem
+          </span>
+      </h1>
+      <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+      </p>
+      <button className='btn btn-primary mx-4'>Visit services
+      <i class="fa-solid fa-arrow-right ms-2"></i>
+            </button>
       <div className="post">
-        <div className="form">
+        {/* <div className="form">
           <form onSubmit={handleSubmit}>
             <div>
               <label htmlFor="image" className="custom-file-label">
@@ -65,8 +78,7 @@ export const Home1 = () => {
             <br />
             <button type="submit">Upload</button>
           </form>
-        </div>
-
+        </div> */}
         {posts.map((post, index) => (
           <div className="postbox" key={index}>
             <img
@@ -77,6 +89,12 @@ export const Home1 = () => {
             <p className="cap">Problem: {post.caption}</p>
           </div>
         ))}
+      </div>
+          </div>
+          <div className='col-6'>
+          <img src={headerImg} className='office-img' />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,49 @@
 import React from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
-import pic from './service_chiyo_logo.jpg'
+import pic from './logo.png'
 
 export const NavBar = () => {
   return (
     <div>
-{/*Navbar*/}
 
-
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#"><img src={pic} alt="" /></a>
+<nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={pic} alt=""/>
+            </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav mx-auto">
               <li className="nav-item">
            <div className="homme">
            <Link className="nav-link " id='home' aria-current="page" to="/Home1">Home</Link>
            </div>
               </li>
               <li className="nav-item">
-                <Link className="nav-link " to="/List">Service-List</Link>
+                <Link className="nav-link " to="/List">Services</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link " to="/Blog">Blog</Link>
               </li>
+              <li className="nav-item">
+                <Link className="nav-link " to="/Blog">Contact</Link>
+              </li>
             </ul>
+            <div>
+              <button className='sign-in'>
+              <i className="fa-regular fa-user me-2">
+                  </i>
+                Sign In
+                </button>
+              <button className='sign-up'>
+                <i className="fa-solid fa-lock me-2">
+                  </i>
+                   Sign Up
+                   </button>
+            </div>
           </div>
         </div>
       </nav>
