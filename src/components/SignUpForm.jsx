e5b9eb79684f8832299aaa4dced50bf75053bce7
BK10 <bk10sandesh@gmail.com>
2023-06-16 17:30:41 +0545

    layout change

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,81 +1,91 @@
-import React, { useState } from 'react'
-import './SignUpForm.css'
+import React, { useState } from "react";
+import "./SignUpForm.css";
 
 export const SignUpForm = () => {
-const [showUserForm , setShowUserForm]= useState(false);
-const [showSupplierForm , setShowSupplierForm]= useState(false);
+  const [showUserForm, setShowUserForm] = useState(false);
+  const [showSupplierForm, setShowSupplierForm] = useState(false);
 
-const handleUserClick =()=> {
+  const handleUserClick = () => {
     setShowUserForm(true);
     setShowSupplierForm(false);
-}
+  };
 
-const handleSupplierClick=()=> {
+  const handleSupplierClick = () => {
     setShowUserForm(false);
     setShowSupplierForm(true);
-}
+  };
 
-const handleFormSubmit =(event)=> {
+  const handleFormSubmit = (event) => {
     event.preventDefault();
-    
-}
-
+  };
 
   return (
     <div>
-        
-    <h1 className='FormHead'>Choose How you want to Sign Up</h1>
-<div className="btn3">
-<button className='btn1' onClick={handleUserClick} >User</button>
-&nbsp;
-&nbsp;
-&nbsp;
-&nbsp;
-<button className='btn2' onClick={handleSupplierClick} >Service Provider</button>
-</div>
+      <h1 className="FormHead">Choose How you want to Sign Up</h1>
+      <div className="btn3">
+        <button className="btn1" onClick={handleUserClick}>
+          User
+        </button>
+        &nbsp; &nbsp; &nbsp; &nbsp;
+        <button className="btn2" onClick={handleSupplierClick}>
+          Service Provider
+        </button>
+      </div>
 
+      {showUserForm && (
+        <form onSubmit={handleFormSubmit}>
+          <div className="formMain">
+            <label className="formLabel">
+              Email:
+              <input type="email" name="email" />
+            </label>
+            <br />
+            <br />
+            <br />
+            <label className="formLabel">
+              Password:
+              <input type="password" name="password" />
+            </label>
+            <br />
+            <button className="submit1" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
 
-{showUserForm && (
-    <form onSubmit={handleFormSubmit}>
-        <label>
-            Email: 
-
-            <input type="email" name='email' />
-        </label>
-        <br />
-        <label>
-            Password: 
-
-            <input type="password" name='password' />
-        </label>
-        <br />
-        <button type='submit'>Submit</button>
-    </form>
-)}
-
-{showSupplierForm && (
-    <form onSubmit={handleFormSubmit}>
-        <label className='name'>
-            Pin: 
-            <input type="number" name='number'/>
-        </label>
-        <br />
-        <label>
-            Email: 
-            <input type="email" name='email' />
-        </label>
-        <br />
-        <br />
-        <br />
-        <label className='name'>
-            password: 
-            <input type="password" name='password' />
-        </label>
-        <br />
-        <button type='submit'>submit</button>
-    </form>
-)}
-
+      {showSupplierForm && (
+        <form onSubmit={handleFormSubmit}>
+          <div className="secondForm">
+          <label className="name">
+            Pin:
+          </label>
+          <br />
+          <br />
+          <input type="number" name="number" />
+          <br />
+          <label className="name">
+            <br />
+            Email:
+            &nbsp;
+          </label>
+          <br />
+          <br />
+          <input type="email" name="email" />
+          <br />
+          <br />
+          <br />
+          <label className="name">
+            password:
+          </label>
+          <br />
+          <br />
+          <input type="password" name="password" />
+          <br />
+          <button className="btn2" type="submit">submit</button>
+          </div>
+        </form>
+      )}
     </div>
-  )
-}
+  );
+};
